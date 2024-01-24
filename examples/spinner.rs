c32c733a4bac5e2264bcd5116138d090072de86e
Roland Schaer <roele33@hotmail.com>
2024-01-24 20:54:28 -0300

    feat: spinner - expose SpinnerStyle and add example (#28)

--- examples/spinner.rs
+++ examples/spinner.rs
@@ -3,11 +3,18 @@ use std::{thread::sleep, time::Duration};
 use demand::{Spinner, SpinnerStyle};
 
 fn main() {
+    let custom_style = SpinnerStyle {
+        frames: vec![
+            "   ", "-  ", "-- ", "---", " --", "  -", "   ", "  -", " --", "---", "-- ", "-  ",
+        ],
+        fps: Duration::from_millis(1000 / 10),
+    };
+
     Spinner::new("Loading Data...")
-        .style(SpinnerStyle::line())
+        .style(&custom_style)
         .run(|| {
             sleep(Duration::from_secs(2));
         })
         .expect("error running spinner");
-    println!("Done!");
+    println!("Data loaded.");
 }
